e8fc8cf027f779653967c5a7e5a87e9eba5ae85a
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-06-01 22:03:42 +0545

    pet-shop by truffle
    pet-shopping

---
+++ contracts/Adoption.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.0 <0.9.0;
+contract Adoption {
+
+    address[16] public adopters;
+    // Adopting a pet
+function adopt(uint petId) public returns (uint) {
+  require(petId >= 0 && petId <= 15);
+
+  adopters[petId] = msg.sender;
+
+  return petId;
+}
+// Retrieving the adopters
+function getAdopters() public view returns (address[16] memory) {
+  return adopters;
+}
+
+
+
+
+}
